8a97ad286b4d7d5235de63747440899b97717597
Feoktist92 <feoktist92@gmail.com>
2023-07-30 21:47:19 +0800

    styles collection-item completed

--- src/components/basketitem.jsx
+++ src/components/basketitem.jsx
@@ -10,35 +10,41 @@ function BasketItem(props) {
     } = props;
     return (
         <li className='collection-item'>
-            <span>{displayName}</span>
-            <i
-                className='material-icons basket-quantity'
-                onClick={() => minusQuantity(mainId)}
-            >
-                remove
-            </i>
-            {'  '}x{quantity}
-            {'  '}
-            <i
-                className='material-icons basket-quantity'
-                onClick={() => plusQuantity(mainId)}
-            >
-                add
-            </i>
-            = <b>{price.regularPrice * quantity}</b> руб.
-            <span
-                className='secondary-content'
-                onClick={() => {
-                    removeFromBasket(mainId);
-                }}
-            >
-                <i
-                    className='material-icons basket-delete'
-                    style={{ color: '#6a1b9a' }}
+            <div className='name'>{displayName}</div>
+            <div className='price'>
+                {' '}
+                <div className='counter'>
+                    <i
+                        className='material-icons basket-quantity'
+                        onClick={() => minusQuantity(mainId)}
+                    >
+                        {' '}
+                        remove
+                    </i>
+                    {'  '}x{quantity}
+                    {'  '}
+                    <i
+                        className='material-icons basket-quantity'
+                        onClick={() => plusQuantity(mainId)}
+                    >
+                        add
+                    </i>
+                </div>
+                <b>{price.regularPrice * quantity}</b> руб.
+                <span
+                    className='secondary-content'
+                    onClick={() => {
+                        removeFromBasket(mainId);
+                    }}
                 >
-                    close
-                </i>
-            </span>
+                    <i
+                        className='material-icons basket-delete'
+                        style={{ color: '#6a1b9a' }}
+                    >
+                        close
+                    </i>
+                </span>
+            </div>
         </li>
     );
 }
